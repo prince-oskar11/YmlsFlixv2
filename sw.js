276e08ced0cb482b8ed369ac9773ef1255af1e8d
prince-oskar11 <prince-oskar11@hotmail.com>
2026-04-09 20:06:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
-const cacheName='ymlanime-cache-v1';
-const assets=['/','/index.html','/manifest.json','/icon.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assets)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)))});
+const CACHE = "ymlanime-v2";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/icon.png"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
